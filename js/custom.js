07be8f3d0a22f0379f0da6f4db323f74a804ac16
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2024-02-26 16:44:59 +0000

    remove unused model

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ var firstPrediction = true;
 // start by asymchronously loading our model and running a dummy
 // prediction to prime the pump.
 (async () => {
-  model = await tf.loadLayersModel('model/model.json')
+  model = await tf.loadLayersModel('models/model1/model.json')
   document.getElementById('model-load').style.display = 'none';
   predict();
 })();
